69ec83e125a42beba0cc09878ea71d35d653f40c
Amzar <amzarirfanrazali@gmail.com>
2026-05-20 00:43:03 +0800

    adding netlify

---
+++ netlify/functions/send.js
@@ -0,0 +1,18 @@
+exports.handler = async function (event) {
+
+  const text = "✅ Test message 2 from Netlify bot!";
+
+  await fetch(`https://api.telegram.org/bot${process.env.TOKEN}/sendMessage`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      chat_id: 344886216,
+      text: text
+    })
+  });
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ success: true })
+  };
+};
